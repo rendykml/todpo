0d54add60e4c8c46844f3c89bd81fed845a85e02
rendykml <rendikamaluddin2018@gmail.com>
2026-02-01 00:40:48 +0700

    push home screen

---
+++ flutter/todpo/lib/features/authentication/screens/home/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import '../../../authentication/screens/login/login.dart';
+import '../../../../utils/token_storage.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        children: [
+          Image(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.height * 0.6,
+            alignment: AlignmentGeometry.center,
+            image: AssetImage(
+              'assets/images/on_boarding_images/onBoarding2.gif',
+            ),
+          ),
+          Text(
+            'selamat datang di todpo',
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 12),
+          Text(
+            'saat nya membuat task dan produktif kembali',
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await TokenStorage.deleteToken();
+
+              if (!context.mounted) return;
+
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (_) => const LoginScreen()),
+                (route) => false,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
