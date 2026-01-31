b3e2bf74984c6eb27fc42a32113253183dbf5884
rendykml <rendikamaluddin2018@gmail.com>
2026-01-31 20:00:06 +0700

    update models user

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  nama: String,
-  email: String
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema(
+  {
+    first_name: { type: String, required: false },
+    last_name: { type: String, required: false },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    nama: String,
+    email: String,
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("User", UserSchema);
